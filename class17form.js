ead38c086a0492c0955a15e133babfdb12c65edd
Prakash0m <omprakash099507@gmail.com>
2024-08-20 17:11:07 +0545

    hel

--- class17form.js
+++ class17form.js
@@ -1,27 +1,21 @@
-const displayMsg=('msg',id,colorName)=>{
-    document.getElementById(id).innerHTML =msg
-    document.getElementById(id).style.color = colorName
-}
+const displayMsg = (msg, id, colorName) => {
+  document.getElementById(id).innerHTML = msg;
+  document.getElementById(id).style.color = colorName;
+};
 
-
-
-
-
-
-
-
-const fnameValidate=()=>{
-    const first_name=document.getElementById('fname').value
-    if(first_name==""){
-        displayMsg('Must provide firstname','fnameMsg','red')
-        return false
-    }
-    else if(first_name.length<3){
-        displayMsg("Must be more than 2 character","fnameMsg",'red')
-        return false
-    }
-    else{
-        displyMsg("Valid First Nmae",'fnameMsg','green')
-        return true
-    }
-}
+const fnameValidate = () => {
+  const first_name = document.getElementById("fname").value;
+  if (first_name == "") {
+    displayMsg("Must provide firstname", "fnameMsg", "red");
+    return false;
+  } else if (first_name.length < 3) {
+    displayMsg("Must be more than 2 character", "fnameMsg", "red");
+    return false;
+  } else if (first_name.match(/^([a-zA-Z])+$/)) {
+    displayMsg("Contain only alphabets", "fnameMsg", "red");
+    return false;
+  } else {
+    displayMsg("Valid First Nmae", "fnameMsg", "green");
+    return true;
+  }
+};
